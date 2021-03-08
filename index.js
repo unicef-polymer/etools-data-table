be442cc3cff9da0917f5420d281fd388ed9ca182
Adriana Trif <trif.corina.adriana@gmail.com>
2021-03-08 17:16:02 +0200

    Separate side effects from package entry point

---
+++ index.js
@@ -0,0 +1,5 @@
+export {EtoolsDataTableColumn} from './etools-data-table-column.js';
+export {EtoolsDataTableRow} from './etools-data-table-row.js';
+export {EtoolsDataTableHeader} from './etools-data-table-header.js';
+export {EtoolsDataTableFooter} from './etools-data-table-footer.js';
+export {dataTableStylesLit} from './data-table-styles-lit.js';
