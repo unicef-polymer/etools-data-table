94030d55a0328b02149a5a7152adc9910b2e6c26
dcioara <dan.cioara@nordlogic.com>
2021-10-18 13:09:07 +0300

    adjustments

--- EtoolsDataTableHeader.js
+++ EtoolsDataTableHeader.js
@@ -65,12 +65,12 @@ export class EtoolsDataTableHeader extends LitElement {
         }
       </style>
 
-      <div id="header-wrapper" part="data-table-header">
-        <div id="title" part="header-title">
+      <div id="header-wrapper" part="edt-data-table-header">
+        <div id="title" part="edt-header-title">
           <span>${this.label}</span>
         </div>
 
-        <div id="columns" part="header-columns">
+        <div id="columns" part="edt-header-columns">
           <slot></slot>
         </div>
       </div>
